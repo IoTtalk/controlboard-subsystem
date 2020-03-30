ea041fda9ca5f8a357499731154ea38d176e8406
Kuan Wen Chen <s103062327@m103.nthu.edu.tw>
2020-03-31 01:19:48 +0800

    correct error, TODO: some_dict

--- cb_manager.py
+++ cb_manager.py
@@ -1,11 +1,13 @@
 from cb_instance import cb_instance
 import models
+
 class cb_manager():
     def init():
         #read cb instance from database
-        users = models.Account.select_all()
-        for user in users:
-            #cb instance initiate with id and device name
+        instances = models.CB_Instances.select_all()
+        for instance in instances:
+            #cb instance initiate with id and device name, make a dict to keep track
+            some_dict[instance.cb_id, instance.cb_name] = cb_instance(instance.cb_id, instance.cb_name)
         return
 
     def create_instance()
