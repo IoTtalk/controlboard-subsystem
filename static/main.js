821b4796071969d340deae7836eee022d473fdb4
luk1684tw <luk1684tw@gmail.com>
2020-10-07 14:23:20 +0800

    remove toggle bar

--- static/main.js
+++ static/main.js
@@ -2,11 +2,15 @@ var app = new Vue({
   el: '#app',
   delimiters: ["<%", "%>"],
   data: {
+    user: {
+      "superuser": 1,
+      "username": "luk1684tw"
+    },
     projects: [
-      {icon: '../static/imgs/landscape.svg', name: 'Hello World'},
-      {icon: '../static/imgs/landscape.svg', name: 'test_1'},
-      {icon: '../static/imgs/landscape.svg', name: 'test_2'},
-      {icon: '../static/imgs/landscape.svg', name: 'test_3'}
+      {icon: "../static/imgs/landscape.svg", name: "Hello World"},
+      {icon: "../static/imgs/landscape.svg", name: "test_1"},
+      {icon: "../static/imgs/landscape.svg", name: "test_2"},
+      {icon: "../static/imgs/landscape.svg", name: "test_3"}
     ]
   }
 
