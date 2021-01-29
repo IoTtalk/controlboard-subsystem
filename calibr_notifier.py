5051dd8edbbded1ae0c19cecc847fc8c0ed5a73e
Kuan Wen Chen <s103062327@m103.nthu.edu.tw>
2021-01-29 16:28:54 +0800

    calibration email notification

--- calibr_notifier.py
+++ calibr_notifier.py
@@ -1,23 +1,3 @@
-calibr_notifier.py
-未共用
-類型
-文字
-大小
-857 個位元組
-儲存空間使用量
-857 個位元組
-位置
-ca
-擁有者
-我
-上次修改時間
-我在2021年1月21日修改過
-上次開啟時間
-我在下午4:24開啟過
-建立日期
-下午4:19 (使用「Google Drive Web」建立)
-新增說明
-檢視者可以下載
 import smtplib
 
 # Import the email modules we'll need
