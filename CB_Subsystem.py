0752bec1b7f2df31ee34bfb1f5b4c0ed5087705f
alisha <alisha618g@gmail.com>
2022-03-28 23:07:40 +0800

    reload flask app when template file changes

--- CB_Subsystem.py
+++ CB_Subsystem.py
@@ -66,6 +66,7 @@ if __name__ == "__main__":
     system_logger.info('Start Launching ControlBoard Subsystem......')
 
     app = Flask(__name__)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
     app.config.update(
         SESSION_COOKIE_SAMESITE=None,
